2d8040c9d51b3b922ff4f5f4ba700dc9533437ac
cvusmo <blacksheepcosmo@gmail.com>
2025-09-29 08:31:17 -0500

    Testing gimp

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Create output directories if they don't exist
-mkdir -p gtk-3.0 gtk-4.0 gnome-shell
+mkdir -p gtk-3.0 gtk-4.0 gnome-shell gimp
 
 # Compile GTK 3.0 variants
 sass src/core/gtk-3.0/gtk-Dark.scss gtk-3.0/gtk-dark.css
@@ -15,8 +15,17 @@ sass src/core/gtk-4.0/gtk-Light.scss gtk-4.0/gtk.css
 sass src/core/gnome-shell/gnome-shell-Dark.scss gnome-shell/gnome-shell.css
 sass src/core/gnome-shell/gnome-shell-Light.scss gnome-shell/gnome-shell-light.css
 
-# Optional: Compile GTK 2.0 (using gtkrc for now, no SASS)
-# Note: GTK 2.0 uses gtkrc files; copy them if needed
+# Compile GIMP variants
+sass src/sass/gtk/apps/_gimp-Light.scss gimp/gimp-light.css
+sass src/sass/gtk/apps/_gimp-Dark.scss gimp/gimp.css
+
+# Symlink to GIMP 3.0 theme dir
+mkdir -p ~/.config/GIMP/3.0/themes/Blackbeard
+ln -sf "$(pwd)/gimp/gimp-dark.css" ~/.config/GIMP/3.0/themes/Blackbeard/gimp-dark.css
+ln -sf "$(pwd)/gimp/gimp.css" ~/.config/GIMP/3.0/themes/Blackbeard/gimp.css
+cp src/core/apps/theme.ini ~/.config/GIMP/3.0/themes/Blackbeard/theme.ini
+
+# Optional: GTK 2.0 (gtkrc)
 mkdir -p gtk-2.0
 cp src/core/gtk-2.0/gtkrc-Dark gtk-2.0/gtkrc
 cp src/core/gtk-2.0/gtkrc gtk-2.0/gtkrc-light
